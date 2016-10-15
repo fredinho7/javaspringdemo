fa8bf59f202fc462468eb38674a23b8f07176066
Fredrik <freddeandersson86@gmail.com>
2016-10-15 12:44:27 +0200

    Created git repo

---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by root on 2016-10-14.
+ */
+
+@RestController
+@RequestMapping(value = "/server/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class DemoController {
+
+    // Endpoint goes here.
+    
+
+
+}
